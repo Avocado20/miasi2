df896e508154f5f472adee62e6e4c44a2723061f
avocado20 <galewskirobert@gmail.com>
2018-10-07 20:49:46 +0200

    agent initialize cleanup

--- src/main/java/AirCraftSystem.java
+++ src/main/java/AirCraftSystem.java
@@ -1,64 +1,58 @@
 import AirPort.AirPortAgent;
 import Aircraft.AirCraftAgent;
 import Weather.WeatherAgent;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@Accessors(chain = true)
 public class AirCraftSystem extends Thread {
 
     private WeatherAgent weatherAgent;
     private AirPortAgent airportAgent;
-    private List<Thread> airCraftAgents = new ArrayList<Thread>();
 
     public void run() {
         this.initializeWeatherAgent();
         this.initAirportAgent();
         this.initializeAirCraftAgents();
-        this.sleep();
     }
 
     protected void initializeWeatherAgent() {
         this.weatherAgent = new WeatherAgent();
-        Thread weatherThread = new Thread(this.weatherAgent);
-        weatherThread.start();
+        new Thread(this.getWeatherAgent()).start();
     }
 
     protected void initAirportAgent() {
         this.airportAgent = new AirPortAgent();
-        Thread airportThread = new Thread(this.airportAgent);
-        airportThread.start();
+        new Thread(this.getAirportAgent()).start();
     }
 
-    protected  void initializeAirCraftAgents() {
+    protected void initializeAirCraftAgents() {
         List<AirCraftAgent> airCrafts = new ArrayList<AirCraftAgent>();
         airCrafts.add(new AirCraftAgent().setID(1).setMaxFuelAmount(200).setCurrentFuelAmount(180).setBasicLandingTime(25).setOnTheFlight(true));
         airCrafts.add(new AirCraftAgent().setID(2).setMaxFuelAmount(300).setCurrentFuelAmount(200).setBasicLandingTime(35).setOnTheFlight(false));
         airCrafts.add(new AirCraftAgent().setID(3).setMaxFuelAmount(400).setCurrentFuelAmount(100).setBasicLandingTime(50).setOnTheFlight(true));
         airCrafts.add(new AirCraftAgent().setID(4).setMaxFuelAmount(600).setCurrentFuelAmount(200).setBasicLandingTime(5).setOnTheFlight(true));
         airCrafts.add(new AirCraftAgent().setID(5).setMaxFuelAmount(800).setCurrentFuelAmount(700).setBasicLandingTime(15).setOnTheFlight(true));
-
-
-       for (AirCraftAgent airCraft : airCrafts) {
-           for (AirCraftAgent other : airCrafts) {
-               if (other != airCraft) {
-                   airCraft.addOtherAgent(other);
-               }
-           }
-           airCraft.setAirPortAgent(this.airportAgent);
-           airCraft.setWeatherAgent(this.weatherAgent);
-           Thread air = new Thread(airCraft);
-           airCraftAgents.add(air);
-           air.start();
-       }
-
+        this.initAirCraftThread(airCrafts);
     }
 
-    protected void sleep() {
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    protected void initAirCraftThread(final List<AirCraftAgent> airCrafts) {
+        for (AirCraftAgent airCraft : airCrafts) {
+            for (AirCraftAgent other : airCrafts) {
+                if (other != airCraft) {
+                    airCraft.addOtherAgent(other);
+                }
+            }
+            airCraft.setAirPortAgent(this.airportAgent);
+            airCraft.setWeatherAgent(this.weatherAgent);
+            new Thread(airCraft).start();
         }
     }
+
 }
